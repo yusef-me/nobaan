301588d380a5a6d2f0a9b330d76b2560f1268e09
yusef-me <yusefmehdizadehen@gmail.com>
2023-02-16 16:41:17 +0330

    feat product card
    product card design and creating a json file for product data

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,9 +1,41 @@
 import React from 'react';
+import "../assets/css/products.scss";
+import {IconButton, Rating} from "@mui/material";
+import ShareIcon from '@mui/icons-material/Share';
+import TurnedInNotIcon from '@mui/icons-material/TurnedInNot';
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import productsData from "../data/products"
 
 const Products = () => {
+    const {Data} = productsData;
     return (
-        <div>
-            products
+        <div className="P-container">
+            {
+                Data.map((product, index) =>
+                    <div className="P-card" key={index} title={product.title}>
+                        <div className="P-card-head">
+                            <img className="p-card-image" src={product.img} alt={product.title}/>
+                            <div className="P-card-action-container">
+                                <IconButton color="primary" aria-label="delete" size="small" title="share">
+                                    <ShareIcon fontSize="inherit"/>
+                                </IconButton>
+                                <IconButton color="primary" aria-label="delete" size="small" title="save">
+                                    <TurnedInNotIcon fontSize="inherit"/>
+                                </IconButton>
+                                <IconButton color="primary" aria-label="delete" size="small" title="add to cart">
+                                    <AddShoppingCartIcon fontSize="inherit"/>
+                                </IconButton>
+                            </div>
+                        </div>
+                        <div className="P-card-body">
+                            <h2>{product.title}</h2>
+                            <small>{product.subTitle}</small>
+                            <Rating name="size-small" defaultValue={product.rating} size="small"/>
+                            <span className="P-card-price">USD<b>{product.price}</b></span>
+                        </div>
+                    </div>
+                )
+            }
         </div>
     );
 };
